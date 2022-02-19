c0608b269f6e36e23166d8ff1b5fd16f25736c7d
Jitesh117 <sahookumarjitesh@gmail.com>
2022-02-19 16:55:29 +0530

    added the javascript form validation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function validateForm() {
     let email = document.forms["myForm"]["email"].value;
     let password = document.forms["myForm"]["password"].value;
     
-    // let wrong = document.getElementsByClassName('wrong');
+    let wrong = document.getElementsByClassName('wrong');
 
     let wrongfname = document.getElementById('wrongfname');
     let wronglname = document.getElementById('wronglname');
@@ -14,22 +14,46 @@ function validateForm() {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (fname == "") {
     //   alert("Name must be filled out");
-    wrongfname.style.display= 'initial'
+    wrongfname.style.display= 'initial';
+    wrong[0].style.display = 'initial'
     //   return false;
+    }
+    else
+    {
+        wrongfname.style.display  = 'none';
+        wrong[0].style.display = 'none'
     }
     if(lname == ""){
         // alert("Last name must be filled out");
         // return false;
         wronglname.style.display = 'initial';
+        wrong[1].style.display = 'initial'
+    }
+    else
+    {
+        wronglname.style.display  = 'none';
+        wrong[1].style.display = 'none'
     }
     if(email == "" || !email.value.match(mailformat)){
         // alert("email must be filled out");
         wrongemail.style.display = 'initial';
+        wrong[2].style.display = 'initial'
         // return false;
+    }
+    else
+    {
+        wrongemail.style.display  = 'none';
+        wrong[2].style.display = 'none'
     }
     if(password == ""){
         // alert("password must be filled out");
         // return false;
         wrongpassword.style.display = 'initial';
+        wrong[3].style.display = 'initial'
+    }
+    else
+    {
+        wrongpassword.style.display  = 'none';
+        wrong[3].style.display = 'none'
     }
   }
